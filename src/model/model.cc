ab666f0fbe0e1a26bcd66299f88268f499ff423d
totalxiao <qianyedaodaozhai@vip.qq.com>
2019-12-16 14:56:26 +0800

    feat: add diffuse texture loader in model

--- src/model/model.cc
+++ src/model/model.cc
@@ -24,21 +24,39 @@ Model::Model(const char* filename) : verts_(), faces_() {
     if (!line.compare(0, 2, "v ")) {
       iss >> trash;
       Vector3f v;
-      for (int i = 0; i < 3; i++)
+      for (int i = 0; i < 3; i++) {
         iss >> v.raw[i];
+      }
       verts_.push_back(v);
+    } else if (!line.compare(0, 3, "vn ")) {
+      iss >> trash >> trash;
+      Vector3f n;
+      for (int i = 0; i < 3; i++) {
+        iss >> n[i];
+      }
+      norms_.push_back(n);
+    } else if (!line.compare(0, 3, "vt ")) {
+      iss >> trash >> trash;
+      Vector2f uv;
+      for (int i = 0; i < 2; i++) {
+        iss >> uv[i];
+      }
+      uv_.push_back(uv);
     } else if (!line.compare(0, 2, "f ")) {
-      std::vector<int> f;
-      int itrash, idx;
+      vector<Vector3i> f;
+      Vector3i tmp;
       iss >> trash;
-      while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-        idx--;  // in wavefront obj all indices start at 1, not zero
-        f.push_back(idx);
+      while (iss >> tmp[0] >> trash >> tmp[1] >> trash >> tmp[2]) {
+        for (int i = 0; i < 3; i++) {
+          tmp[i]--;  // in wavefront obj all indices start at 1, not zero
+        }
+        f.push_back(tmp);
       }
       faces_.push_back(f);
     }
   }
-  std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
+  std::cout << "# v# " << verts_.size() << " f# " << faces_.size() << " vt# "
+            << uv_.size() << " vn# " << norms_.size() << std::endl;
 }
 
 Model::~Model() {}
@@ -51,10 +69,44 @@ int Model::nfaces() {
   return (int)faces_.size();
 }
 
-std::vector<int> Model::face(int idx) {
-  return faces_[idx];
+vector<int> Model::face(int idx) {
+  vector<int> face;
+  vector<Vector3i> faces = faces_[idx];
+  for (int i = 0, size = (int)faces.size(); i < size; i++) {
+    face.push_back(faces[i][0]);
+  }
+  return face;
 }
 
 Vector3f Model::vert(int i) {
   return verts_[i];
+}
+
+void Model::load_texture(const string& filename,
+                         const char* suffix,
+                         TGAImage& img) {
+  std::string texfile(filename);
+  size_t dot = texfile.find_last_of(".");
+  if (dot != std::string::npos) {
+    texfile = texfile.substr(0, dot) + std::string(suffix);
+    std::cerr << "texture file " << texfile << " loading "
+              << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed")
+              << std::endl;
+    img.flip_vertically();
+  }
+}
+
+TGAColor Model::diffuse(Vector2i uv) {
+  return diffusemap_.get(uv.x, uv.y);
+}
+
+Vector2i Model::uv(int iface, int nvert) {
+  int idx = faces_[iface][nvert][1];
+  return Vector2i(uv_[idx].x * diffusemap_.get_width(),
+                  uv_[idx].y * diffusemap_.get_height());
+}
+
+Vector3f Model::norm(int iface, int nvert) {
+  int idx = faces_[iface][nvert][2];
+  return norms_[idx].normalize();
 }
--- src/model/model.h
+++ src/model/model.h
@@ -1,23 +1,33 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
+#include "../lib/tga/tga_image.h"
 #include "../util/geometry.h"
 #include <vector>
 
+using std::string;
 using std::vector;
 
 class Model {
 private:
   vector<Vector3f> verts_;
-  vector<vector<int>> faces_;
+  vector<vector<Vector3i>> faces_;
+  vector<Vector3f> norms_;
+  vector<Vector2f> uv_;
+  TGAImage diffusemap_;
 
 public:
   Model(const char* filename);
   ~Model();
   int nverts();
   int nfaces();
+  Vector3f norm(int iface, int nvert);
   Vector3f vert(int i);
+  Vector2i uv(int iface, int nvert);
+  TGAColor diffuse(Vector2i uv);
   vector<int> face(int idx);
+
+  void load_texture(const string& filename, const char* suffix, TGAImage& img);
 };
 
 #endif  //__MODEL_H__
